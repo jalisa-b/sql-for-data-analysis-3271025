ce8e425e7652ce215ad922ba69bcf49db574a02f
jalisa-b <78390413+jalisa-b@users.noreply.github.com>
2023-08-27 23:03:07 +0000

    added comments to SelectTop10Customers

--- SelectTop10Customers.sql
+++ SelectTop10Customers.sql
@@ -2,14 +2,22 @@
 -- the total number a customer
 -- has ever spent with H + Sport
 -- and display the 10 customers who purchased the most
+
+-- select customer id, first name, last name, and sum of total due
 SELECT Customer.CustomerId,
   FirstName,
   LastName,
   Sum(TotalDue)
+-- from customer table
 FROM Customer
+-- join to orders table on customer id
   JOIN Orders ON Customer.CustomerId = Orders.CustomerId
+-- group by customer id, first name, last name
+-- group by groups rows that have the same values
 GROUP BY Customer.CustomerId,
   FirstName,
   LastName
+-- order by sum of total due in descending order
 ORDER BY Sum(TotalDue) DESC
+-- limit to top 10 results
 LIMIT 10
